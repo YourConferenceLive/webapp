883fb5b5b78880c60e135061027c02058f66744d
athulak <athullive@gmail.com>
2021-06-04 04:11:11 +0200

    Removing unused method

--- application/views/themes/default_theme/admin/sponsors/create-sponsor-modal.php
+++ application/views/themes/default_theme/admin/sponsors/create-sponsor-modal.php
@@ -135,19 +135,4 @@
 		});
 	}
 
-	function imagePreview(input)
-	{
-		if (input.files && input.files[0])
-		{
-			let reader = new FileReader();
-
-			reader.onload = function (e)
-			{
-				element.next('.image-preview').attr('src', e.target.result);
-			}
-
-			reader.readAsDataURL(input.files[0]);
-		}
-	}
-
 </script>
