607f8a00a2691ecdf99c34d790e582e8dc6a7c1c
athulak <athullive@gmail.com>
2021-06-07 09:18:02 +0200

    Misc

--- application/views/themes/default_theme/attendee/common/menu-bar.php
+++ application/views/themes/default_theme/attendee/common/menu-bar.php
@@ -12,7 +12,15 @@ $ci_method = $this->router->fetch_method();
 </style>
 
 <header>
-	<nav id="mainMenu" class="navbar navbar-expand-md navbar-light <?=($ci_controller == 'sessions' && $ci_method == 'view')?'':'fixed-top'?> bg-white">
+	<nav
+			id="mainMenu"
+			class="navbar navbar-expand-md navbar-light bg-white
+			<?=
+			(
+					($ci_controller == 'sessions' && $ci_method == 'view') ||
+					($ci_controller == 'sponsor' && $ci_method == 'index')
+			)?'':'fixed-top'?>
+			">
 		<a class="navbar-brand" href="#"><img src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/theme_assets/logo.png" alt="<?=$this->project->name?> Logo" onerror="this.src='<?=ycl_root?>/ycl_assets/ycl_logo.png'" style="max-width: 80px;"></a>
 		<button class="navbar-toggler collapsed navbar-light" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -42,6 +50,28 @@ $ci_method = $this->router->fetch_method();
 				</li>
 			</ul>
 			<ul class="navbar-nav">
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<span class="badge badge-pill badge-primary" style="float:right;margin-bottom:-10px;">2</span>
+						<i class="fas fa-envelope" style="color: #487391;"></i>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">
+							<img class="person-icon-small" src="https://franchisematch.com/wp-content/uploads/2015/02/john-doe-300x300.jpg">
+							<span>John Doe</span><br>
+							<small style="margin-left: 30px;">Sample message...</small>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+<!--							<img class="person-icon-small" src="--><?//=ycl_root?><!--/ycl_assets/images/person-icon.png">-->
+							<img class="person-icon-small" src="https://fwcdn.pl/ppo/01/59/159/449673.2.jpg">
+							<span>Daniel Day-Lewis</span><br>
+							<small style="margin-left: 30px;">Sample message 2...</small>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#"><strong>See all messages</strong></a>
+					</div>
+				</li>
 				<li class="nav-item">
 					<a class="nav-link disabled" href="#"><?=$user['name']?></a>
 				</li>
