9ff9bc59413ff65267773f193b1111f3f45d28fb
athulak <athullive@gmail.com>
2021-06-10 09:13:06 +0200

    Evaluation init

---
+++ application/controllers/public/attendee/Evaluation.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Evaluation extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		if (!isset($_SESSION['project_sessions']["project_{$this->project->id}"]) || $_SESSION['project_sessions']["project_{$this->project->id}"]['is_attendee'] != 1)
+			redirect(base_url().$this->project->main_route."/login"); // Not logged-in
+
+		$this->load->model('Logger_Model', 'logger');
+	}
+
+	public function index()
+	{
+		$this->logger->log_visit("Evaluation");
+
+		$data['project'] = $this->project;
+		$data['user'] = $_SESSION['project_sessions']["project_{$this->project->id}"];
+
+		$this->load
+			->view("{$this->themes_dir}/{$this->project->theme}/attendee/common/header", $data)
+			->view("{$this->themes_dir}/{$this->project->theme}/attendee/common/menu-bar", $data)
+			->view("{$this->themes_dir}/{$this->project->theme}/attendee/evaluation", $data)
+			->view("{$this->themes_dir}/{$this->project->theme}/attendee/common/footer", $data)
+		;
+	}
+}
---
+++ application/views/themes/default_theme/attendee/evaluation.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<style>
+	body
+	{
+		background-color: #eaeaea;
+	}
+</style>
