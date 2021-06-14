2edb1698deb7937353fea355a7bd35c611eb7578
athulak <athullive@gmail.com>
2021-06-14 04:42:45 +0200

    Lounge background change

--- application/views/themes/default_theme/attendee/lounge.php
+++ application/views/themes/default_theme/attendee/lounge.php
@@ -3,4 +3,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <link href="<?=ycl_root?>/theme_assets/<?=$this->project->theme?>/css/lobby.css?v=<?=rand()?>" rel="stylesheet">
 
-<img id="full-screen-background" src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/other_images/lounge.png">
+<img id="full-screen-background" src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/other_images/lounge.jpeg">
