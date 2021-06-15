ba9b776189a07e18393df27d44b2ee0687787b1f
hayreddintuzel <hayreddintuzel@gmail.com>
2021-06-15 11:01:55 +0300

    lobby

--- application/views/themes/default_theme/attendee/common/header.php
+++ application/views/themes/default_theme/attendee/common/header.php
@@ -31,6 +31,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="<?=ycl_root?>/vendor_frontend/toastr/toastr.js"></script>
 	<script src="<?=ycl_root?>/vendor_frontend/toastr/toastr.config.js"></script>
 
+	<!--Google Font-->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 	<!-- Theme style and js -->
 	<link href="<?=ycl_root?>/theme_assets/<?=$this->project->theme?>/css/app.css?v=2" rel="stylesheet">
