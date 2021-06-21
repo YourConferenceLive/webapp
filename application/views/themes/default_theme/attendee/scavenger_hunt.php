ded70a0d8ddd89bd2364fd5739267ae7c5e5635d
athulak <athullive@gmail.com>
2021-06-21 08:51:59 +0200

    Extra margin on top

--- application/views/themes/default_theme/attendee/scavenger_hunt.php
+++ application/views/themes/default_theme/attendee/scavenger_hunt.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		background-color: #2a2a2e;
 	}
 </style>
-<object id="shPdf" data="<?=ycl_root?>/cms_uploads/projects/3/theme_assets/scavenger_hunt/Scavenger_Hunt_Instructions.pdf" type="application/pdf" width="100%" height="900px">
+<object id="shPdf" data="<?=ycl_root?>/cms_uploads/projects/3/theme_assets/scavenger_hunt/Scavenger_Hunt_Instructions.pdf" type="application/pdf" width="100%" height="900px" style="margin-top: 69px;">
 	<p>It appears you don't have a PDF plugin for this browser.
 		You can <a target="_blank" href="<?=ycl_root?>/cms_uploads/projects/3/theme_assets/scavenger_hunt/Scavenger_Hunt_Instructions.pdf">click here to download the PDF file.</a></p>
 </object>
