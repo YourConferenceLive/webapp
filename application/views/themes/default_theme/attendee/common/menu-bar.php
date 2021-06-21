c053cf96bf7dce6c17986152888d255bf8639afe
athulak <athullive@gmail.com>
2021-06-21 08:39:27 +0200

    🧑 Profile image in the header

--- application/views/themes/default_theme/attendee/common/menu-bar.php
+++ application/views/themes/default_theme/attendee/common/menu-bar.php
@@ -6,6 +6,21 @@ $ci_method = $this->router->fetch_method();?>
 		.navbar-light .navbar-nav .nav-link {
 			color: rgb(72, 115, 145);
 		}
+		.profile-dp {
+			border-radius: 50%;
+			float: left;
+			height: 30px;
+			width: 30px;
+		}
+		ul.navbar-nav li.dropdown:hover > div.dropdown-menu {
+			display: block;
+		}
+		@media (min-width: 979px) {
+			ul.navbar-nav li.dropdown:hover > div.dropdown-menu {
+				display: block;
+			}
+		}
+
 	</style>
 	<header>
 		<nav id="mainMenu" class="navbar navbar-expand-md navbar-light bg-white <?=(($ci_controller == 'sessions' && $ci_method == 'view') || ($ci_controller == 'sponsor' && $ci_method == 'index'))?'':'fixed-top'?>">
@@ -47,7 +62,18 @@ $ci_method = $this->router->fetch_method();?>
 							<a class="dropdown-item" href="#"><strong>See all messages</strong></a>
 						</div>
 					</li>
-					<li class="nav-item"><a class="nav-link disabled" href="#"><?=$user['name']?></a></li>
+
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img class="profile-dp" src="<?=ycl_root?>/cms_uploads/user_photo/profile_pictures/<?=$user['photo']?>" onerror="this.onerror=null;this.src='<?=ycl_root?>/ycl_assets/images/person_dp_placeholder.png';" alt="DP">
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="<?=base_url($this->project->main_route)?>/profile" style="color: rgb(72, 115, 145) !important;"><i class="far fa-id-card"></i> Profile</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="<?=base_url($this->project->main_route)?>/authentication/logout" style="color: rgb(72, 115, 145) !important;">Logout <i class="nav-icon fas fa-sign-out-alt"></i></a>
+						</div>
+					</li>
+
 				</ul>
 			</div>
 		</nav>
