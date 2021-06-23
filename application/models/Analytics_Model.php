b70e8545eb9d4de2fa1bbe0bf2903c49c52fb65d
athulak <athullive@gmail.com>
2021-06-23 21:31:41 +0200

    Rexter's mistake merge

--- application/models/Analytics_Model.php
+++ application/models/Analytics_Model.php
@@ -38,4 +38,29 @@ class Analytics_Model extends CI_Model
 			return $result->result();
 		return new stdClass();
 	}
+
+	public function getScavengerHuntData()
+	{
+
+		$this->db->select('
+		user.id as user_id, user.name as user_fname, user.surname as user_surname, user.email, 
+		 max(scavenger_hunt_items.date) as last_collected, scavenger_hunt_items.icon_name')
+			->from('scavenger_hunt_items')
+			->join('user','user.id = scavenger_hunt_items.user_id')
+			->join('sponsor_booth','sponsor_booth.id = scavenger_hunt_items.booth_id')
+			->where('scavenger_hunt_items.user_id IN 
+						(
+						SELECT user_id
+						FROM scavenger_hunt_items 
+						GROUP BY user_id
+						HAVING COUNT(user_id) >= 10
+						)
+					')
+		;
+		$result = $this->db->get();
+		if ($result->num_rows() > 0)
+			return $result->result();
+
+		return new stdClass();
+	}
 }
