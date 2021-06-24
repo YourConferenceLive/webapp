9fd2e714c015b9214aa17925146e0c84d2fe1e36
athulak <athullive@gmail.com>
2021-06-24 09:30:10 +0200

    Moment JS

--- application/views/themes/default_theme/attendee/common/header.php
+++ application/views/themes/default_theme/attendee/common/header.php
@@ -33,7 +33,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="<?=ycl_root?>/vendor_frontend/toastr/toastr.config.js"></script>
 
 	<!-- Moment JS -->
+	<!-- https://momentjs.com/docs/#/displaying/ -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.33/moment-timezone-with-data.min.js" integrity="sha512-rjmacQUGnwQ4OAAt3MoAmWDQIuswESNZwYcKC8nmdCIxAVkRC/Lk2ta2CWGgCZyS+FfBWPgaO01LvgwU/BX50Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 	<!--Google Font-->
 	<link rel="preconnect" href="https://fonts.gstatic.com">
