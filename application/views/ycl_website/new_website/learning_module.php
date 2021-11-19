e954b557e9c1363ee58642a658958c03c0b910aa
onemarkgit <94550567+onemarkgit@users.noreply.github.com>
2021-11-18 16:04:52 -0800

    Add files via upload

---
+++ application/views/ycl_website/new_website/learning_module.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  <head>
+    <!-- Site Title-->
+    <title>Learning Modules</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+   <link rel="icon" href="https://dev.yourconference.live/ycl_assets/ycl_icon.png">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Libre+Franklin:200,300,500,600,300italic">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+    <!-- Page -->
+    <div class="page">
+      <div id="page-loader">
+        <div class="cssload-container">
+          <div class="cssload-speeding-wheel"></div>
+        </div>
+      </div>
+      <!-- Page header-->
+      <header class="page-header">
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar novi-background bg-cover" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-sm-device-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-device-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-stick-up-clone="false" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-lg-stick-up-offset="69px" data-xl-stick-up-offset="1px" data-xxl-stick-up-offset="1px">
+            <div class="rd-navbar-inner">
+              <!-- RD Navbar Panel -->
+              <div class="rd-navbar-panel">
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                <!-- RD Navbar Brand-->
+                <div class="rd-navbar-brand"><a class="brand-name" href="index.html"><img src="images/ycl_logo_header.png" alt="" width="163" height="40"/></a></div>
+              </div>
+              <!-- RD Navbar Nav-->
+              <div class="rd-navbar-nav-wrap">
+                <div class="rd-navbar-nav-wrap__element">
+                      <ul class="rd-navbar-nav">
+                        <li><a href="index.html">Home</a>
+                       </li>
+                        <li><a href="services.html">Services</a>
+                          <ul class="rd-navbar-dropdown">
+                            <li><a href="webinars.html">Webinars</a></li>
+              <li><a href="virtual.html">Virtual Conferences</a></li>
+                <li><a href="hybrid.html">Hybrid Events</a></li>
+                            </li>
+                          </ul>
+                        </li>
+                        <li><a href="contacts.html">Contact</a>
+                        </li>
+				  <li><a class="brand-name" href="special.html"><img src="images/first_free.jpg" alt="" width="163" height="40"/></a>
+                      </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <section class="breadcrumbs-custom novi-background bg-cover">
+        <div class="container">
+          <div class="breadcrumbs-custom__inner">
+            <p class="breadcrumbs-custom__title">Professional Webinar Production & Broadcasting</p>
+            <ul class="breadcrumbs-custom__path">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="services.html">Services</a></li>
+              <li class="active">Webinars</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="text-center">
+        <!-- RD Parallax-->
+        <div class="parallax-container bg-image parallax-header rd-parallax-light" data-parallax-img="images/parallax-05.jpg">
+          <div class="parallax-content">
+            <div class="parallax-header__inner">
+              <div class="parallax-header__content">
+                <div class="container">
+                  <div class="row justify-content-sm-center">
+                    <div class="col-md-10 col-xl-8">
+                      <h2>Professional and Precise</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-lg bg-white text-center novi-background bg-cover">
+        <div class="container">
+          <div class="row row-50 justify-content-sm-center">
+            <div class="col-md-10">
+              <h5>Audience Response</h5>
+              <p>Experience virtually instant, real-time connection with your audience, with none of the long awkward pauses between polls and speakers that are common among other providers. We keep your audience engaged by providing the lowest latency between live speakers and viewers. This means more attendees stay on longer. And that means more data for you to know the effectiveness of your program.</p>
+            </div>
+            <div class="col-md-10">
+              <h5>Stylish Interface</h5>
+              <p>Good design is not only pleasant on the eye, it helps users absorb information quicker. </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      </section>
+      <section class="bg-accent section-lg novi-background bg-cover">
+        <div class="container">
+          <div class="row row-fix">
+            <div class="col-md-4">
+              <h3>Take the Next Step...</h3>
+            </div>
+            <div class="col-md-8">
+              <p> YCL has everything to you need for a successful event. Let our experienced team help evaluate your objectives and put together a comprehensive plan that will impress your membership.</p><a class="button button-gray-light-outline" href="shannon-profile.html">Contact Shannon</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      </section>
+      <!-- Page Footer -->
+      <section class="pre-footer-corporate novi-background bg-cover">
+        <div class="container">
+          <div class="row justify-content-sm-center justify-content-lg-start row-30 row-md-60">
+            <div class="col-sm-10 col-md-6 col-lg-10 col-xl-3">
+              <h6>About</h6>
+              <p>Your Conference DOT Live is created and owned by One World Presentation Management Ltd., the world leader in innovative presentation management solutions for life science conferences.</p>
+            </div>
+            <div class="col-sm-10 col-md-6 col-lg-3 col-xl-3">
+              <h6>Products</h6>
+              <ul class="list-xxs">
+                <li><a href="#">Webinars</a></li>
+              <li><a href="#">Virtual Conferences</a></li>
+                <li><a href="#">Hybrid Events</a></li>
+              </ul>
+            </div>
+            
+            <div class="col-sm-10 col-md-6 col-lg-4 col-xl-3">
+              <h6>Bricks & Mortar</h6>
+              <ul class="list-xs">
+                <li>
+                  <dl class="list-terms-minimal">
+                    <dt>Location:</dt>
+                    <dd>Vancouver, BC, Canada</dd>
+                  </dl>
+                </li>
+                <li>
+                  <dl class="list-terms-minimal">
+                    <dt>Toll free:</dt>
+                    <dd>
+                      <ul class="list-semicolon">
+                        <li><a href="tel:#">(800) 952-3259</a></li>
+                        
+                      </ul>
+                    </dd>
+                  </dl>
+                </li>
+                <li>
+                  <dl class="list-terms-minimal">
+                    <dt>E-mail</dt>
+                    <dd><a href="mailto:#">info@yourconference.live</a></dd>
+                  </dl>
+                </li>
+                <li>
+                  <dl class="list-terms-minimal">
+                    <dt>We are open</dt>
+                    <dd>Mn-Fr: 10 am-8 pm</dd>
+                  </dl>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer class="footer-corporate novi-background bg-cover">
+        <div class="container">
+          <div class="footer-corporate__inner">
+            <p class="rights"><span>YCL</span><span>&nbsp;</span><span class="copyright-year"></span>. Design&nbsp;by&nbsp;<a class="link-primary-inverse" href="https:www.owpm.com">OWPM</a></p>
+            <ul class="list-inline-xxs">
+              <li><a class="novi-icon icon icon-xxs icon-primary fa fa-facebook" href="#"></a></li>
+              <li><a class="novi-icon icon icon-xxs icon-primary fa fa-twitter" href="#"></a></li>
+   
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
