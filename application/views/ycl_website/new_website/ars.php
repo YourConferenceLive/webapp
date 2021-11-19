63878094b71280514c0d87554bd79eff1cb5fe47
onemarkgit <94550567+onemarkgit@users.noreply.github.com>
2021-11-18 16:16:49 -0800

    Update ars.php

--- application/views/ycl_website/new_website/ars.php
+++ application/views/ycl_website/new_website/ars.php
@@ -10,8 +10,8 @@
    <link rel="icon" href="https://dev.yourconference.live/ycl_assets/ycl_icon.png">
     <!-- Stylesheets-->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Libre+Franklin:200,300,500,600,300italic">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?=base_url()?>ycl_assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?=base_url()?>ycl_assets/css/style.css">
 		<!--[if lt IE 10]>
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <script src="js/html5shiv.min.js"></script>
