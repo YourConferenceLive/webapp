1ec1bfd7e56435cb5a632186f3e073c2f4af2e6a
Rexter Dayuta <rexterdayuta@gmail.com>
2021-12-21 02:08:15 +0800

    Revert "COS logo update"
    This reverts commit f5166019eff3fbd57e7d2bae49a9f7fc71920f0c.

--- application/views/themes/default_theme/sponsor/common/menu-bar.php
+++ application/views/themes/default_theme/sponsor/common/menu-bar.php
@@ -21,7 +21,7 @@ $ci_method = $this->router->fetch_method();
 					($ci_controller == 'sponsor' && $ci_method == 'index')
 			)?'':'fixed-top'?>
 			">
-		<a class="navbar-brand" href="#"><img src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/theme_assets/logo.png" alt="<?=$this->project->name?> Logo" onerror="this.src='<?=ycl_root?>/ycl_assets/ycl_logo.png'" style="max-width: 120px;"></a>
+		<a class="navbar-brand" href="#"><img src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/theme_assets/logo.png" alt="<?=$this->project->name?> Logo" onerror="this.src='<?=ycl_root?>/ycl_assets/ycl_logo.png'" style="max-width: 80px;"></a>
 		<button class="navbar-toggler collapsed navbar-light" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
