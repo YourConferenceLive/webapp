be949aed8b7fd650090a4bb038d795e0f32ce707
Rexter Dayuta <rexterdayuta@gmail.com>
2023-01-25 19:17:53 +0800

    admin poll comparison id

--- application/views/themes/ccs/admin/sessions/polls.php
+++ application/views/themes/ccs/admin/sessions/polls.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									<th>Poll ID</th>
 									<th>Name</th>
 									<th>Type</th>
+									<th>Comparison ID</th>
 									<th>Auto-show Result</th>
 									<th>Poll Triggers</th>
 									<th>Result Triggers</th>
@@ -384,6 +385,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					'	</td>' +
 					'	<td>' +
 					'		'+poll.poll_type+
+					'	</td>' +
+					'	<td>' +
+					'		'+((poll.poll_comparison_id !=='0')? poll.poll_comparison_id :'')+
 					'	</td>' +
 					'	<td>' +
 					'		'+show_result+
