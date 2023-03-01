c98b34e15481e5bba7fd4fcefc627d08ed4a1009
Rexter Dayuta <rexterdayuta@gmail.com>
2023-03-01 22:59:53 +0800

    sidebar presenter logo removed + presenter link fix

--- application/views/themes/cea/presenter/common/sidebar.php
+++ application/views/themes/cea/presenter/common/sidebar.php
@@ -4,8 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
-	<a href="<?=$this->project_url.'/admin/dashboard'?>" class="brand-link">
-		<img src="<?=ycl_root?>/cms_uploads/projects/<?=$this->project->id?>/theme_assets/logo.png" alt="Logo" class="brand-image <?=(($this->project->name)=='Demo')?'':'img-circle'?>" style="opacity: .8 <?=(($this->project->name=="Demo"))?'max-width:120px':''?>">
+	<a href="<?=$this->project_url.'/presenter/dashboard'?>" class="brand-link">
+	<!--	<img src="<?/*=ycl_root*/?>/cms_uploads/projects/<?/*=$this->project->id*/?>/theme_assets/logo.png" alt="Logo" class="brand-image <?/*=(($this->project->name)=='Demo')?'':'img-circle'*/?>" style="opacity: .8 <?/*=(($this->project->name=="Demo"))?'max-width:120px':''*/?>">
+	-->
 		<span class="brand-text font-weight-light"><?=$this->project->name?> Presenter</span>
 	</a>
 
