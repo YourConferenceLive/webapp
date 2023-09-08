f1a98e496b7b5d3bba8d3771937b9368fd226ad5
Rexter Dayuta <rexterdayuta@gmail.com>
2023-09-09 03:02:50 +0800

    Revert "Update list.php"
    This reverts commit bbd66a7985c27cc0b35c587df7b38b11a0809ca6.

--- application/views/themes/cea/admin/sessions/list.php
+++ application/views/themes/cea/admin/sessions/list.php
@@ -923,24 +923,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
 									}
 								})
 							});
-						},
-						 success: function(data) {
-							console.log(data)
-							if (data == 1) {
-								Swal.fire({
-									text: "",
-									icon: 'success',
-									title: successText
-								})
-							} else {
-								Swal.fire({
-									text: "",
-									icon: 'info',
-									title: infoText
-								})
-							}
-						},
-					})
+						}
+					}).done(function(result) {
+	
+						result = JSON.parse(result)
+						console.log('result is'+ result)
+						if (result == 1) {
+							Swal.fire({
+								text: "",
+								icon: 'success',
+								title: successText
+							})
+						} else {
+							Swal.fire({
+								text: "",
+								icon: 'info',
+								title: infoText
+							})
+						}
+					});
 				}
 			})
 			
